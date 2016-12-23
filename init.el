f5aed074e5189c556fa34502b6c0f77ead7f5e9c
pmariani <pierre.mariani@gmail.com>
2016-12-23 13:15:42 -0800

    Create init.el

---
+++ init.el
@@ -0,0 +1,98 @@
+;; Start the daemon with /Applications/Emacs.app/Contents/MacOS/Emacs-x86_64-10_9 --daemon
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Backup and Temporary files
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Custom file in separate location
+(setq custom-file "~/.emacs.d/custom-file.el")
+(load custom-file)
+
+(column-number-mode t)
+(desktop-save-mode t)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(set-cursor-color "magenta")
+(setq confirm-kill-emacs 'yes-or-no-p)
+(setq sentence-end-double-space nil) ; Sentences end with one space
+(setq visible-bell t)
+(show-paren-mode 1)
+(tool-bar-mode -1)
+
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-x C-z"))
+(global-unset-key (kbd "s-w"))  ;; AppleKey-w: delete-frame
+(global-unset-key (kbd "s-p"))  ;; AppleKey-p: ns-print-buffer
+(global-unset-key (kbd "s-t"))  ;; AppleKey-t: ns-popup-font-panel
+
+;; Transparency
+(add-to-list 'default-frame-alist '(alpha . (89 . 70)))
+
+(use-package ibuffer
+  :bind ("C-x C-b" . ibuffer))
+
+(use-package ido
+  :init (setq ido-enable-flex-matching t) (setq ido-everywhere t)
+  :config (ido-mode 1))
+
+(use-package tramp
+  :init (setq tramp-default-method "ssh"))
+
+(use-package ace-window
+  :ensure t
+  :bind ("C-x o" . ace-window))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package flycheck-pyflakes
+  :ensure t)
+
+;; Python:
+;; brew install python3
+;; virtualenv --python python3 ~/.emacs.d/python_venv
+;; source ~/.emacs.d/python_venv/bin/activate
+;; pip install --upgrade pip
+;; pip install jedi flake8 importmagic
+(use-package elpy
+  :ensure t
+  :init
+  (setq elpy-modules '(elpy-module-company
+		       elpy-module-eldoc
+		       elpy-module-highlight-indentation
+		       elpy-module-pyvenv
+		       elpy-module-flymake
+		       elpy-module-sane-defaults))
+  (pyvenv-activate "~/.emacs.d/python_venv")
+  
+  :config
+  (add-hook 'elpy-mode-hook (lambda ()
+			      (hs-minor-mode t)
+			      (set-fill-column 100)))
+  (elpy-enable))
+
+(defun copy-region-to-os-pasteboard ()
+  (interactive)
+  (shell-command-on-region
+   (region-beginning)
+   (region-end)
+   "pbcopy"))
+
+(global-set-key (kbd "<f8>") 'copy-region-to-os-pasteboard)
