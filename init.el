1c6e9bd39bb14e61485dd07c0eb7f75e2468d507
Pierre Mariani <pierre@energysavvy.com>
2017-09-27 07:29:48 -0700

    bunch of refactor and cleanup

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (set-language-environment "UTF-8")
-(customize-set-variable 'exec-path (append exec-path '("/usr/local/bin")))
+
 (require 'package)
 (setq package-enable-at-startup nil)
 
@@ -10,7 +10,7 @@
 
 (package-initialize)
 
-;; Bootstrap `use-package'
+;; Bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -27,14 +27,15 @@
 
 (column-number-mode t)
 (desktop-save-mode t)
+(show-paren-mode t)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(set-cursor-color "magenta")
-(setq confirm-kill-emacs 'yes-or-no-p)
-(setq sentence-end-double-space nil) ; Sentences end with one space
-(setq visible-bell t)
-(show-paren-mode 1)
 (tool-bar-mode -1)
+
+(customize-set-variable 'exec-path (append exec-path '("/usr/local/bin")))
+(customize-set-variable 'confirm-kill-emacs 'yes-or-no-p)
+(customize-set-variable 'sentence-end-double-space nil) ; Sentences end with one space
+(customize-set-variable 'visible-bell t)
 
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-z"))
@@ -43,15 +44,24 @@
 (global-unset-key (kbd "s-t"))  ;; AppleKey-t: ns-popup-font-panel
 (global-unset-key (kbd "C-t"))  ;; Transpose chars
 
+(use-package dracula-theme
+  :ensure t
+  :config (set-cursor-color "magenta"))
+
+(use-package beacon
+  :ensure t)
+
 (use-package ibuffer
   :bind ("C-x C-b" . ibuffer))
 
 (use-package ido
-  :init (setq ido-enable-flex-matching t) (setq ido-everywhere t)
+  :init (progn
+	  (customize-set-variable 'ido-enable-flex-matching t)
+	  (customize-set-variable 'ido-everywhere t))
   :config (ido-mode 1))
 
 (use-package tramp
-  :init (setq tramp-default-method "ssh"))
+  :init (customize-set-variable 'tramp-default-method "ssh"))
 
 (use-package ace-window
   :ensure t
@@ -73,10 +83,10 @@
   :ensure t)
 
 (use-package dired
-  :init
-  (add-hook 'after-init-hook '(lambda () (require 'dired-x)))
-  (customize-set-variable 'dired-omit-files "^\\.?#\\|^\\.$\\|^__pycache__$")
-  (setq dired-omit-mode t))
+  :init (progn
+	  (add-hook 'after-init-hook '(lambda () (require 'dired-x)))
+	  (customize-set-variable 'dired-omit-files "^\\.?#\\|^\\.$\\|^__pycache__$")
+	  (setq dired-omit-mode t)))
 
 (use-package json-mode
   :ensure t)
@@ -87,27 +97,30 @@
 ;; source ~/.emacs.d/python_venv/bin/activate
 ;; pip install --upgrade pip
 ;; pip install jedi flake8 importmagic
+;;
+;; in .dir-locals.el:
+;; ((python-mode
+;;  (pyvenv-activate . "~/.emacs.d/python_venv")))
+
 (use-package elpy
   :ensure t
-  :init
-  (setq elpy-modules '(elpy-module-company
-		       elpy-module-eldoc
-		       elpy-module-highlight-indentation
-		       elpy-module-pyvenv
-		       elpy-module-flymake
-		       elpy-module-sane-defaults))
-  (pyvenv-activate "~/.emacs.d/python_venv")
+  :init (progn
+	  (setq elpy-modules '(elpy-module-company
+			       elpy-module-eldoc
+			       elpy-module-highlight-indentation
+			       elpy-module-pyvenv
+			       elpy-module-flymake
+			       elpy-module-sane-defaults)))
 
-  :config
-  (add-hook 'elpy-mode-hook (lambda ()
-			      (hs-minor-mode t)
-			      (set-fill-column 100)
-			      (font-lock-add-keywords nil
-						      '(("\\<\\(\\(FIXME\\)\\|\\(TODO\\)\\|\\(NOTE\\)\\):" 1 font-lock-warning-face prepend)))))
-  (elpy-enable))
-
-(customize-set-variable 'elpy-disable-backend-error-display nil)
-
+  :config (progn
+	    (add-hook 'elpy-mode-hook (lambda ()
+					(pyvenv-mode t)
+					(hs-minor-mode t)
+					(set-fill-column 100)
+					(font-lock-add-keywords nil
+								'(("\\<\\(\\(FIXME\\)\\|\\(TODO\\)\\|\\(NOTE\\)\\):" 1 font-lock-warning-face prepend)))))
+	    (customize-set-variable 'elpy-disable-backend-error-display nil)
+	    (elpy-enable)))
 
 (defun copy-region-to-os-pasteboard ()
   (interactive)
@@ -143,15 +156,11 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 
 (customize-set-variable 'org-default-notes-file "~/org/notes.org")
-(customize-set-variable 'org-capture-templates
-			'(
-			  ("n" "Captured Note" entry (file+headline org-default-notes-file "Captured Notes") "")))
+(customize-set-variable 'org-capture-templates '(
+						 ("n" "Captured Note" entry (file+headline org-default-notes-file "Captured Notes") "")))
 (customize-set-variable 'epg-gpg-program "/usr/local/bin/gpg2") ;; add pinentry-mode loopback to ~/.gnupg/gpg.conf
 (customize-set-variable 'org-crypt-key "")
 (customize-set-variable 'org-tags-exclude-from-inheritance '("crypt"))
-
-(use-package org-crypt
-  :config (org-crypt-use-before-save-magic))
 
 (customize-set-variable 'safe-local-variable-values '((buffer-auto-save-file-name)))
 
@@ -255,35 +264,50 @@
 
 (use-package mu4e
   :load-path "/usr/local/share/emacs/site-lisp/mu/mu4e/"
-  :config
-  (customize-set-variable 'send-mail-function           'smtpmail-send-it)
-  (customize-set-variable 'mu4e-maildir                 "~/.Mail")
-  (customize-set-variable 'mu4e-get-mail-command        "/usr/local/bin/offlineimap")
-  (customize-set-variable 'mu4e-sent-messages-behavior  'delete)
-  (customize-set-variable 'mu4e-show-images             t)
-  (customize-set-variable 'mu4e-update-interval         300)
-  (customize-set-variable 'smtpmail-default-smtp-server "smtp.gmail.com")
-  (customize-set-variable 'smtpmail-smtp-server         "smtp.gmail.com")
-  (customize-set-variable 'user-full-name               "Pierre Mariani")
-  (customize-set-variable 'smtpmail-smtp-service        587)
-  (customize-set-variable 'smtpmail-stream-type         'starttls)
-  (customize-set-variable 'message-kill-buffer-on-exit  t)
-  (customize-set-variable 'mu4e-bookmarks               (make-bookmarks *ACCOUNT-DEFINITIONS*))
-  (customize-set-variable 'mu4e-contexts                (mapcar #'make-account-context *ACCOUNT-DEFINITIONS*))
-  (add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t))
+  :config (progn
+	    (customize-set-variable 'send-mail-function           'smtpmail-send-it)
+	    (customize-set-variable 'mu4e-maildir                 "~/.Mail")
+	    (customize-set-variable 'mu4e-get-mail-command        "/usr/local/bin/offlineimap")
+	    (customize-set-variable 'mu4e-sent-messages-behavior  'delete)
+	    (customize-set-variable 'mu4e-show-images             t)
+	    (customize-set-variable 'mu4e-update-interval         300)
+	    (customize-set-variable 'smtpmail-default-smtp-server "smtp.gmail.com")
+	    (customize-set-variable 'smtpmail-smtp-server         "smtp.gmail.com")
+	    (customize-set-variable 'user-full-name               "Pierre Mariani")
+	    (customize-set-variable 'smtpmail-smtp-service        587)
+	    (customize-set-variable 'smtpmail-stream-type         'starttls)
+	    (customize-set-variable 'message-kill-buffer-on-exit  t)
+	    (customize-set-variable 'mu4e-bookmarks               (make-bookmarks *ACCOUNT-DEFINITIONS*))
+	    (customize-set-variable 'mu4e-contexts                (mapcar #'make-account-context *ACCOUNT-DEFINITIONS*))
+	    (add-to-list 'mu4e-view-actions '("ViewInBrowser" . mu4e-action-view-in-browser) t)))
 
 (use-package mu4e-alert
   :ensure t
   :after mu4e
-  :init
-  (customize-set-variable 'mu4e-alert-interesting-mail-query (unread-messages-query *ACCOUNT-DEFINITIONS*))
-  (mu4e-alert-set-default-style 'notifier)
-  (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-  (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display))
+  :init (progn
+	  (customize-set-variable 'mu4e-alert-interesting-mail-query (unread-messages-query *ACCOUNT-DEFINITIONS*))
+	  (mu4e-alert-set-default-style 'notifier)
+	  (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+	  (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)))
 
 ;;;;;;;;;;;;
-;; Racket ;;
+;; more packages ;;
 ;;;;;;;;;;;;
 
 (use-package racket-mode
   :ensure t)
+
+(use-package browse-kill-ring
+  :ensure t
+  :config (browse-kill-ring-default-keybindings))
+
+(use-package paredit
+  :ensure t
+  :config (progn
+	    (autoload 'enable-paredit-mode "paredit")
+	    (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)))
+
+(use-package mode-icons
+  :ensure t
+  :config (mode-icons-mode t))
+
