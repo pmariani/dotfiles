9fb273eacc0e019c4cf4812c4dcea617b22308f4
Pierre Mariani <pierre@energysavvy.com>
2018-01-17 11:51:41 -0800

    foo

--- init.el
+++ init.el
@@ -1,12 +1,12 @@
 (set-language-environment "UTF-8")
 
+(require 'cl-lib)
 (require 'package)
+
 (setq package-enable-at-startup nil)
 
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives
-             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 
 (package-initialize)
 
@@ -31,6 +31,8 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(set-frame-parameter (selected-frame) 'alpha '(90 . 75))  ;; transparency
+(add-to-list 'default-frame-alist '(alpha . (90 . 75)))  ;; transparency
 
 (customize-set-variable 'exec-path (append exec-path '("/usr/local/bin")))
 (customize-set-variable 'confirm-kill-emacs 'yes-or-no-p)
@@ -43,10 +45,6 @@
 (global-unset-key (kbd "s-p"))  ;; AppleKey-p: ns-print-buffer
 (global-unset-key (kbd "s-t"))  ;; AppleKey-t: ns-popup-font-panel
 (global-unset-key (kbd "C-t"))  ;; Transpose chars
-
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (customize-set-variable 'indent-tabs-mode nil)))
 
 (use-package dracula-theme
   :ensure t
@@ -95,6 +93,30 @@
 (use-package json-mode
   :ensure t)
 
+(use-package browse-kill-ring
+  :ensure t
+  :config (browse-kill-ring-default-keybindings))
+
+(use-package paredit
+  :ensure t
+  :config (progn
+            (autoload 'enable-paredit-mode "paredit")
+            (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+            (add-hook 'emacs-lisp-mode-hook (lambda ()
+                                              (customize-set-variable 'indent-tabs-mode nil)))))
+
+(use-package mode-icons
+  :ensure t
+  :config (mode-icons-mode t))
+
+(use-package smex
+  :ensure t
+  :config (progn
+            (global-set-key (kbd "M-x") 'smex)
+            (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+            ;; This is your old M-x.
+            (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
+
 ;; python:
 ;; brew install python3
 ;; virtualenv --python python3 ~/.emacs.d/python_venv
@@ -126,33 +148,6 @@
             (customize-set-variable 'elpy-disable-backend-error-display nil)
             (elpy-enable)))
 
-(defun copy-region-to-os-pasteboard ()
-  (interactive)
-  (shell-command-on-region
-   (region-beginning)
-   (region-end)
-   "pbcopy"))
-
-(global-set-key (kbd "<f8>") 'copy-region-to-os-pasteboard)
-
-;; Make Tramp use the PATH of the remote user
-(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
-
-;; History of input
-(defvar history-of-vagrant-directories nil)
-(eval-after-load "savehist"
-  '(add-to-list 'savehist-additional-variables 'history-of-vagrant-directories))
-
-;; Function to run tox
-(defun run-tox-on-dir (directory-name)
-  (interactive
-   (list (read-from-minibuffer "run tox on " (car history-of-vagrant-directories) nil nil 'history-of-vagrant-directories)))
-  (let ((default-directory (concat "/ssh:vagrant:/vagrant/" directory-name)))
-    (shell-command "tox &")))
-
-;; Keybinding for tox
-(global-set-key (kbd "C-t") 'run-tox-on-dir)
-
 ;; Org mode
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
@@ -162,24 +157,19 @@
 (customize-set-variable 'org-default-notes-file "~/org/notes.org")
 (customize-set-variable 'org-capture-templates '(
                                                  ("n" "Captured Note" entry (file+headline org-default-notes-file "Captured Notes") "")))
-(customize-set-variable 'epg-gpg-program "/usr/local/bin/gpg2") ;; add pinentry-mode loopback to ~/.gnupg/gpg.conf
-(customize-set-variable 'org-crypt-key "")
 (customize-set-variable 'org-tags-exclude-from-inheritance '("crypt"))
-
 (customize-set-variable 'safe-local-variable-values '((buffer-auto-save-file-name)))
-
 
 ;;;;;;;;;;
 ;; MU4E ;;
 ;;;;;;;;;;
 ;; Had to manually edit offlineimap to force it to use python2.7
-(require 'cl-lib)
 
 ;;; Helper functions
 (when (fboundp 'imagemagick-register-types)
   (imagemagick-register-types))
 
-(defun mac-open-ff (url &optional _)
+(defun mac-open-perso (url &optional _)
   (start-process (concat "opening in firefox " url) nil "open" "-a" "Firefox" url))
 
 (defun mac-open-chrome (url &optional _)
@@ -254,9 +244,8 @@
 (setq *ACCOUNT-DEFINITIONS*
       (list (make-account :email-address "foo"
                           :name          "Personal"
-                          :browser-func  'mac-open-ff
-                          :folders       (list (make-folder :name "BJJ log"       :maildir "/[Gmail].bjj_log"       :shortcut ?b)
-                                               (make-folder :name "Workout log"   :maildir "/[Gmail].workout_log"   :shortcut ?w)
+                          :browser-func  'mac-open-perso
+                          :folders       (list (make-folder :name "Workout log"   :maildir "/[Gmail].workout_log"   :shortcut ?w)
                                                (make-folder :name "House remodel" :maildir "/[Gmail].house_remodel" :shortcut ?h)))
             (make-account :email-address "bar"
                           :name          "Work"
@@ -296,26 +285,5 @@
           (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
           (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)))
 
-;;;;;;;;;;;;
-;; more packages ;;
-;;;;;;;;;;;;
-
-(use-package racket-mode
-  :ensure t)
-
-(use-package browse-kill-ring
-  :ensure t
-  :config (browse-kill-ring-default-keybindings))
-
-(use-package paredit
-  :ensure t
-  :config (progn
-            (autoload 'enable-paredit-mode "paredit")
-            (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)))
-
-(use-package mode-icons
-  :ensure t
-  :config (mode-icons-mode t))
-
-;; Start it
+;; Start mu4e
 (mu4e)
