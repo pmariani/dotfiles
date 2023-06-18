9439316a6b828d221fdd666ad6c0d3ebb75836cc
pmariani <222020+pmariani@users.noreply.github.com>
2023-06-17 18:15:35 -0700

    Update init.el

--- init.el
+++ init.el
@@ -1,8 +1,26 @@
 ;;; package --- Summary
-
 ;;; Commentary:
-
+;; C-u M-x align-regexp \(\s-*\)\s-
+;; M-;   to add comment end of line
+;; M-x occur is cool!
+;; M-x highlight-phrase then unhighlight-xx
+;; C-c C-/ comment-or-uncomment-region
+;; C-x C-= to change font size
+;; C-c . org mode insert date
+;; <f1> open-init.el
+;; <f2> hs-toggle-hiding
+;; <f3> hs-hide-all
+;; <f4> toggle-truncate-lines
+;; <f5> start-debugging
+;; <f6> my-whitespace-cleanup
+;; <f12> imenu
+;; M-m compile
+;; In ibuffer use comma (',') to rotate through different sorting
+;; shr-render-buffer to render HTML
+;; M-x list-face-display and look for font-lock-X-face
+;; hide-ifdef-toggle-shadowing
 ;;; Code:
+
 (set-language-environment "UTF-8")
 
 (require 'cl-lib)
@@ -20,6 +38,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; This is to benchmark startup time.
+;; (use-package benchmark-init
+;;   :ensure t
+;;   :config
+;;   ;; To disable collection of benchmark data after init is done.
+;;   (add-hook 'after-init-hook 'benchmark-init/deactivate))
+
 ;; Backup and Temporary files
 (setq create-lockfiles nil)  ;; Prevent annoying elpy errors
 (setq backup-directory-alist
@@ -30,18 +55,23 @@
 ;; Custom file in separate location
 (setq custom-file "~/.emacs.d/custom-file.el")
 (load custom-file)
+(setq gc-cons-threshold 100000000)
 
 (column-number-mode t)
 (show-paren-mode t)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(setq-default global-visual-line-mode t)
-(set-frame-parameter (selected-frame) 'alpha '(95 . 75))  ;; transparency
-(add-to-list 'default-frame-alist '(alpha . (95 . 75)))  ;; transparency
+
+;; Allows to switch buffers in case-insensitive fashion.
+(setq read-buffer-completion-ignore-case t)
+;; (desktop-save-mode 1)
+;; (savehist-mode 1)
+;; (setq-default global-visual-line-mode t)
 (customize-set-variable 'confirm-kill-emacs 'yes-or-no-p)
 (customize-set-variable 'sort-fold-case t)
-(customize-set-variable 'sentence-end-double-space nil) ; Sentences end with one space
+(customize-set-variable 'sentence-end-double-space nil) ; Sentences end with one
+                                                        ; space
 (customize-set-variable 'visible-bell nil)
 
 (setq ring-bell-function (lambda ()
@@ -50,170 +80,131 @@
 
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-z"))
-(global-unset-key (kbd "s-w"))  ;; AppleKey-w: delete-frame
-(global-unset-key (kbd "s-p"))  ;; AppleKey-p: ns-print-buffer
-(global-unset-key (kbd "s-t"))  ;; AppleKey-t: ns-popup-font-panel
 (global-unset-key (kbd "C-t"))  ;; Transpose chars
-(set-face-attribute 'default nil :font "Monaco-11")
+(global-unset-key (kbd "<f1>"))
 
-(set-fill-column 130)
-(global-prettify-symbols-mode t)
-
-(font-lock-add-keywords nil '(("\\<\\(\\(FIXME\\)\\|\\(TODO\\)\\|\\(NOTE\\)\\):" 1 font-lock-warning-face prepend)))
-
-(use-package rjsx-mode
-  :ensure t
-  :config (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode)))
-
-(use-package web-mode
-  :ensure t
-  :config (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
-
-(eval-and-compile
-  (defvar-local beautiful-themes '(dracula-theme
-                                   creamsody-theme
-                                   warm-night-theme
-                                   purple-haze-theme
-                                   ample-theme
-                                   darktooth-theme))
-
-  (defvar-local selected-theme (nth
-                                (random (length beautiful-themes))
-                              beautiful-themes))
-  (message (concat "Selecting " (symbol-name selected-theme)))
-
-  (defmacro enable-selected-theme ()
-    "Yeah yeah."
-    `(use-package ,selected-theme
-       :ensure t)))
-
-(enable-selected-theme)
-
-(use-package ibuffer
-  :bind ("C-x C-b" . ibuffer))
-
-(use-package ido
-  :init (progn
-          (customize-set-variable 'ido-enable-flex-matching t)
-          (customize-set-variable 'ido-everywhere t))
-  :config (ido-mode 1))
-
-(use-package tramp
-  :init (customize-set-variable 'tramp-default-method "ssh"))
-
-(use-package ace-window
-  :ensure t
-  :bind ("C-x o" . ace-window))
-
-(use-package markdown-mode
-  :ensure t
-  :config
-  (add-hook 'markdown-mode-hook (lambda () (set-fill-column 100))))
-
-(use-package yaml-mode
-  :ensure t)
-
-(use-package company
-  :ensure t
-  :config (global-company-mode))
-
-(use-package flycheck
-  :ensure t
-  :config (progn
-            (customize-set-variable 'flycheck-emacs-lisp-load-path 'inherit)
-            (global-flycheck-mode)))
-
-(use-package flycheck-cask
-  :ensure t
-  :config (add-hook 'flycheck-mode-hook 'flycheck-cask-setup))
-
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status)
-  :config (progn
-            (setq magit-commit-show-diff nil)
-            (put 'magit-clean 'disabled nil)
-            (add-hook 'magit-status-sections-hook 'magit-insert-worktrees)))
-
-(use-package git-gutter
-  :ensure t
-  :config (global-git-gutter-mode 't)
-  :diminish git-gutter-mode)
+(add-to-list 'auto-mode-alist '("\\.razor\\'" . csharp-mode))
+(add-to-list 'auto-mode-alist '("\\.ts" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.shader" . c++-mode))
+(setq js-jsx-detect-syntax nil)
 
 (use-package dired
   :init (progn
           (add-hook 'after-init-hook (lambda () (require 'dired-x)))
-          (customize-set-variable 'dired-omit-files "^\\.?#\\|^\\.$\\|^__pycache__$")
-          (add-hook 'dired-mode-hook (lambda () (dired-omit-mode)))))
+          ;; (customize-set-variable 'dired-omit-files "^\\.?#\\|^\\.$\\|^__pycache__$")
+          ;; (add-hook 'dired-mode-hook (lambda () (dired-omit-mode)))
+          ))
 
-(use-package json-mode
-  :ensure t)
+(setq whitespace-line-column 80)
+(setq-default fill-column 80)
+;; (global-display-fill-column-indicator-mode)
+;; (global-whitespace-mode) ;; show whitespace characters
+(setq sgml-basic-offset 2) ;; For HTML+ indentation
+(setq js-indent-level 2)
+(global-display-line-numbers-mode)
+(setq display-line-numbers-type 'relative)
+
+(add-to-list 'default-frame-alist '(font . "consolas-11" ))
+(set-face-attribute 'default t :font "consolas-11" )
+
+(use-package ibuffer
+  :bind ("C-x C-b" . ibuffer))
 
 (use-package browse-kill-ring
   :ensure t
   :config (browse-kill-ring-default-keybindings))
 
-(use-package paredit
-  :ensure t
-  :config (progn
-            (autoload 'enable-paredit-mode "paredit")
-            (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-            (add-hook 'emacs-lisp-mode-hook (lambda ()
-                                              (customize-set-variable 'indent-tabs-mode nil)))))
-
-(use-package mode-icons
-  :ensure t
-  :config (mode-icons-mode t))
-
-(use-package smex
-  :ensure t
-  :config (progn
-            (global-set-key (kbd "M-x") 'smex)
-            (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-            ;; This is your old M-x.
-            (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
-
-'(use-package nyan-mode
-  :ensure t
-  :config (progn (nyan-mode 1)
-                 (customize-set-variable 'nyan-animate-nyancat t)
-                 (customize-set-variable 'nyan-wavy-trail t)))
-
 (use-package whitespace-cleanup-mode
   :ensure t
+  ;; Enable whitespace-cleanup-mode on save
   :config (global-whitespace-cleanup-mode t))
-
-(use-package exec-path-from-shell
-  :ensure t
-  :config (exec-path-from-shell-initialize))
-
-'(use-package moody
-  :ensure t
-  :config (progn
-            (setq x-underline-at-descent-line t)
-            (moody-replace-mode-line-buffer-identification)
-            (moody-replace-vc-mode)))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-(use-package org
-  :defer t
-  :bind (("C-c l" . org-store-link)
-         ("C-c a" . org-agenda)
-         ("C-c c" . org-capture)
-         ("C-c b" . org-iswitchb))
-  :config (progn
-            (org-babel-do-load-languages 'org-babel-load-languages
-                                         '((shell . t)))
-            (setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "|" "DONE" "WON'T-DO")))
-            (customize-set-variable 'org-default-notes-file "~/org/notes.org")
-            (customize-set-variable 'org-capture-templates '(("n"
-                                                              "Captured Note"
-                                                              entry
-                                                              (file+headline org-default-notes-file "Captured Notes")
-                                                              "")))))
+(setq-default indent-tabs-mode nil)
+(setq-default c-basic-offset 4)
+
+(defun open-init.el ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; (defun set-screen-size ()
+;;   (interactive)
+;;   (when window-system
+;;     (set-frame-size (selected-frame) 150 40)
+;;     (set-frame-position (selected-frame) 20 0)))
+
+(defun my-whitespace-cleanup ()
+  (interactive)
+  (whitespace-cleanup)
+  (message "cleaned whitespace.."))
+
+(defun start-debugging ()
+  (interactive)
+  (message "start debugging..")
+  (shell-command ".\\start-debugging"))
+
+(use-package cc-mode :init (progn
+                             (customize-set-variable 'c-default-style "bsd")
+                             (customize-set-variable 'compilation-read-command nil)
+                             (customize-set-variable 'compile-command ".\\build")
+                             (global-set-key (kbd "C-c C-/") 'comment-or-uncomment-region)
+                             (global-set-key (kbd "<f1>") 'open-init.el)
+                             (global-set-key (kbd "<f2>") 'hs-toggle-hiding)
+                             (global-set-key (kbd "<f3>") 'hs-hide-all)
+                             (global-set-key (kbd "<f4>") 'toggle-truncate-lines)
+                             (global-set-key (kbd "<f5>") 'start-debugging)
+                             (global-set-key (kbd "<f6>") 'my-whitespace-cleanup)
+                             (global-set-key (kbd "<f12>") 'imenu)
+                             (global-set-key (kbd "M-m") 'compile)))
+
+(load-theme 'pierre)
+(global-hl-line-mode 1)
+
+;; (use-package autumn-light-theme :ensure t)
+;; (use-package faff-theme :ensure t)
+;; (use-package forest-blue-theme :ensure t)
+;; (use-package material-theme :ensure t)
+;; (use-package nofrils-acme-theme :ensure t)
+;; (use-package poet-theme :ensure t)
+;; (use-package reykjavik-theme :ensure t)
+;; (use-package subatomic-theme :ensure t)
+;; (use-package toxi-theme :ensure t)
+;; (use-package zweilight-theme :ensure t)
+
+;; (use-package sublime-themes :ensure t)
+;;   :init (progn
+;;           ;; (load-theme 'junio)
+;;           ;; (load-theme 'fogus)
+;;           ;; (load-theme 'manoj-dark) (global-hl-line-mode 0)
+;;           ;; (load-theme 'granger)
+;;           ;; (load-theme 'wombat)
+;;           ;; (set-cursor-color "violet")
+;;           ))
+
+(defun highlight-todos ()
+   (font-lock-add-keywords nil
+                           '(("\\<\\(FIXME\\):" 1 font-lock-warning-face t)
+                             ("\\<\\(TODO\\):" 1 font-lock-warning-face t)
+                             ("\\<\\(DIRTY\\):" 1 font-lock-warning-face t)                             
+                             ("\\<\\(HACK\\):" 1 font-lock-warning-face t)
+                             ("\\<\\(BUG\\):" 1 font-lock-warning-face t)
+                             ("\\<\\(NOTE\\):" 1 font-lock-doc-face t)
+                             )))
+(add-hook 'prog-mode-hook 'highlight-todos)
+(add-hook 'prog-mode-hook (lambda () (hs-minor-mode)))
+(add-hook 'prog-mode-hook (lambda () (hide-ifdef-mode)))
+
+;; The original value is "\f\\|[      ]*$", so we add the bullets (-), (+), and (*).
+;; There is no need for "^" as the regexp is matched at the beginning of line.
+(setq paragraph-start "\f\\|[ \t]*$\\|[ \t]*[-+*] ")
+
+(setq hs-allow-nesting t)
 
 (server-start)
 
-(provide 'init)
-;;; init.el ends here
+
+
+
+(provide '.emacs)
+;;; .emacs ends here
